8cfe2bb051841b55b707ce023a92c4e1fae36f05
Luisa <luhcotmel@gmail.com>
2024-02-11 12:51:28 -0300

    fix/email

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function toggleTheme() {
     }
 }
 
+function email() {
+    document.getElementById("email").addEventListener("click", function () {
+        // Endereço de email para o destinatário
+        var destinatario = "luhcotmel@gmail.com";
+
+        // Assunto do email
+        var assunto = "Parcerias com O PROJETO";
+
+        // Corpo do email
+        var corpoEmail = "";
+
+        // Monta o link de email com os dados
+        var linkEmail = "mailto:" + destinatario +
+            "?subject=" + encodeURIComponent(assunto) +
+            "&body=" + encodeURIComponent(corpoEmail);
+
+        // Abre o cliente de email padrão com os dados preenchidos
+        window.location.href = linkEmail;
+    });
+};
+email();
+
